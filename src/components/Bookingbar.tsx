a421e47a27b3b061aa1e40ec5f108436103a811d
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-26 16:14:14 +0545

    added booking box

---
+++ src/components/Bookingbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BookingBar = () => {
+  return (
+    <div className="Booking-card">
+        <div className="search">
+            <input type="text" placeholder="Search for packages..." />
+            <button>Search</button>
+        </div>
+        <div className="no-of-people">
+            <label htmlFor="people">No. of People:</label>
+            <input type="number" id="people" min="1" defaultValue="1" />
+        </div>
+        <div className="booking-button">
+            <button>Book Now</button>
+        </div>
+    </div>
+  );
+};
+
+export default BookingBar;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-
-import logo from "../assets/logo.png";
+import logo from "../assets/Group 16.png";
 
 const Navbar = () => {
   return (
