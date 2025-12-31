1a90303706c5b56c012046fd7ecd20567d1463da
Raghab Neupane <neupaneraghab@gmail.com>
2025-12-31 10:34:51 +0545

    made the ui for hotel card

--- src/sections/Hotels.tsx
+++ src/sections/Hotels.tsx
@@ -1,6 +1,9 @@
 import HorizontalScroll from '../components/HorizontalScroll';
 import Hc from '../components/hotelcard/hc'
-import Everest from '../assets/Everest.webp';
+import Pokhara from '../assets/hotels_image/Pokhara.webp';
+import Kathmandu from '../assets/hotels_image/Kathmandu.webp';
+import Lumbini from '../assets/hotels_image/HotelLumbiniHeritage.jpg';
+import Mustang from '../assets/hotels_image/Lo_Mustang_Resort.jpg'
 
 const Hotels = () => {
   return (
@@ -9,21 +12,27 @@ const Hotels = () => {
         <HorizontalScroll>
 
           <Hc
-            image={Everest}
+            image={Kathmandu}
             title="Kathmandu Marriott Hotel"
             description="A luxurious stay in the heart of Kathmandu with world-class amenities."
           />
 
           <Hc
-            image={Everest}
-            title="Pokhara"
-            description="A beautiful city known for its stunning lakes and mountains."
+            image={Pokhara}
+            title="Hotel ABC Pokhara"
+            description="Look for the stunning lakes and mountains residing with us!"
           />
 
           <Hc
-            image={Everest}
-            title="Lumbini"
-            description="The birthplace of Lord Buddha, a UNESCO World Heritage Site."
+            image={Lumbini}
+            title="Hotel Lumbini Heritage"
+            description="Experience the rich history of Lumbini with luxurious accommodations."
+          />
+
+          <Hc
+            image={Mustang}
+            title="Hotel Lo Mustang Resort"
+            description="Experience the unique culture and stunning landscapes of Mustang."
           />
 
         </HorizontalScroll>
